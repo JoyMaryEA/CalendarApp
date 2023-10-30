8d3bd0ec763c92e3084a3bdd1716fb9a3fc7d67b
Joy Mary Jepkosgei <joy.jepkosgei@evidenceaction.org>
2023-10-30 15:20:36 +0300

    fixes foldure structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { AuthGuard } from './Guards/auth.guard';
 import { LoginComponent } from './login/login.component';
-import { AuthGuard } from './auth.guard';
 
 
 const routes:Routes =[
