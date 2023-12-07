6c7f3849ecb5426f3b91b2ecc76a3d45755e5bc4
JoyMaryEA <joy.jepkosgei@evidenceaction.org>
2023-12-07 11:06:26 +0300

    same as previous

---
+++ src/app/Interfaces/index.ts
@@ -0,0 +1,10 @@
+
+
+export interface IuserLogin{
+    success:string,
+    token:string,
+    u_id:string
+}
+export interface error{
+    error:string
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,18 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LoginService } from '../Services/login.service';
+import { FormBuilder, FormGroup,  ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
  
-   
-  onSubmit(){
-    const username = document.querySelector("#username") as HTMLInputElement 
-    localStorage.setItem('username',username.value)
+  constructor(private loginService:LoginService){}
+  getUsername(email:string): string{
+    const name= email.split(".")
+    console.log(name);
+    return name[0]
   }
+  onSubmit(event: Event){
+    event.preventDefault()
+    const email = document.querySelector("#email") as HTMLInputElement 
+    const password = document.querySelector("#password") as HTMLInputElement 
+    console.log(email.value,password.value);
+    this.loginService.login({email:email.value, password:password.value}).subscribe( response => {
+      console.log(response);
+      console.log(response.success); 
+      
+      localStorage.setItem("token",response.token)
+    },
+    error => {
+      console.log(error.error.error); //to get msg as string
+      
+      
+    })
+    const username = this.getUsername(email.value)
+    localStorage.setItem('username',username)
+    
+  }
+
+
 }
