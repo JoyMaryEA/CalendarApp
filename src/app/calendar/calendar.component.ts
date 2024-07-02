1dd7830a27c6e059e56312abf6fbfcfb0a5323ec
JoyMaryEA <joy.jepkosgei@evidenceaction.org>
2024-07-02 10:23:43 +0300

    allows selecting dates

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,19 +1,56 @@
-import { Component} from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import { Component } from '@angular/core';
+import { CalendarOptions, EventInput } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 import { FullCalendarModule } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [ FullCalendarModule],
+  imports: [FullCalendarModule],
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent {
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin]
-  };
-}
+    plugins: [dayGridPlugin, interactionPlugin],
+    selectable: true,
+    select: this.handleDateSelect.bind(this),
+    events: [
+      { title: 'Event 1', start: '2024-07-04' },
+    ],
+    eventClick: (info) => {
+      console.log('Event clicked:', info.event);
 
+      const modalContent = `
+        <h2>Event Details</h2>
+        <p>Title: ${info.event.title}</p>
+        <p>Start Date: ${info.event.startStr}</p>
+      `;
+
+      alert(modalContent);
+
+      info.el.style.borderColor = 'red';
+    }
+  };
+
+  handleDateSelect(info: { startStr: string; endStr: string; }) {
+    console.log('Selected date:', info.startStr);
+
+    const newEventTitle = prompt('Enter event title:', '');
+    if (newEventTitle) {
+      const newEvent: EventInput = {
+        title: newEventTitle,
+        start: info.startStr,
+        end: info.endStr || info.startStr
+      };
+
+      // Update the events array immutably
+      this.calendarOptions.events = [
+        ...(this.calendarOptions.events as EventInput[]),
+        newEvent
+      ];
+    }
+  }
+}
