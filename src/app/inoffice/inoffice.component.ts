4a6869f65f40335121b64c12a273e30f63652723
JoyMaryEA <joy.jepkosgei@evidenceaction.org>
2024-08-04 13:36:15 +0300

    total seats

--- src/app/inoffice/inoffice.component.ts
+++ src/app/inoffice/inoffice.component.ts
@@ -18,7 +18,7 @@ export class InofficeComponent implements OnInit, OnDestroy{
   constructor(private router:Router,private userInfoService: UserInfoService,private dataService: DataServiceService){}
   currentPun!: string;
   currentDate!: string;
-  totalSeats: number = 14;
+  totalSeats: number = 13;
   data: any;
   private refreshSubscription!: Subscription;
   puns: string[] = [
